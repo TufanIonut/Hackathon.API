96f930fc0d903dfa8fd7d38c1e39c53b3b6e2e4d
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2023-10-28 13:58:03 +0300

    classes created

---
+++ Hackathon.API/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Hackathon.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.API.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        
+        [Route("/api/Login")]
+        [HttpPost]
+        public IActionResult LoginUser([FromBody]UserCredentials userCredentials)
+        {
+
+            return Ok("Login Successfull");
+        }
+
+    }
+}

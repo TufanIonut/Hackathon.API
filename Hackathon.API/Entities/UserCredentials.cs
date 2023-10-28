54159c8c856a5db379da63f1d8f21ff473df62af
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2023-10-28 15:41:59 +0300

    finish api login

--- Hackathon.API/Entities/UserCredentials.cs
+++ Hackathon.API/Entities/UserCredentials.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+using Hackathon.API.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
 namespace Hackathon.API.Entities
 {
@@ -8,8 +11,26 @@ namespace Hackathon.API.Entities
         public int IdUser { get; set; }
         public string EmailAdress { get; set; }
 
-        public string Password { get; set; }
-        
+        public string UserPassword { get; set; }
+
         public List<UserRoles> userRoles { get; set; }
+
+        public bool CheckLoginCredentials(UserCredentials credentials)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("Email", credentials.EmailAdress);
+            parameters.Add("Password", credentials.UserPassword);
+            parameters.Add("IsValid", DbType.Boolean, direction: ParameterDirection.Output);
+            DataBaseConnection dataBaseConnection = new DataBaseConnection();
+            var connection = dataBaseConnection.ConnectToDataBase();
+            connection.Execute("CheckLoginCredentials", parameters, commandType: CommandType.StoredProcedure);
+            bool isValid = parameters.Get<int>("IsValid") == 1;
+
+            if (isValid)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }

a9714c4b0556519cc50b38845ea8b5438e135875
Tufan Ionut-Petrisor <117408976+TufanIonut@users.noreply.github.com>
2023-10-28 16:56:35 +0300

    RegisterController

---
+++ Hackathon.API/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using Hackathon.API.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.API.Controllers
+{
+    [ApiController]
+    [Route("/api/Register")]
+    public class RegisterController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult RegisterUser([FromBody]UserDTO userCredentials)
+        {
+            if(userCredentials.CheckLoginCredentials(userCredentials))
+            { 
+                return BadRequest("Sorry, an account with this email adress already exists");
+            }
+            else
+            {
+                userCredentials.RegisterUser(userCredentials);
+                return Ok("Registration Succesful");
+            }
+            
+        }
+    }
+}
